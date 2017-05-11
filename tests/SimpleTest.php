88f0c9f9b6365fb3cbebaa7ce4fdf4b6a08483a2
Nathawat Thumthiwong <n.thumthiwong@gmail.com>
2017-05-11 22:29:08 +0700

    pass test

--- tests/SimpleTest.php
+++ tests/SimpleTest.php
@@ -4,8 +4,9 @@ class SimpleTest extends \PHPUnit_Framework_TestCase
 {
      public function testSimple1()
      {
-      $this->assertEquals(2, 1 + 2);
+      $this->assertEquals(2, 1 + 2); 
      }
+
      public function testSimple2()
      {
       $this->assertEquals(2, 1+1);
